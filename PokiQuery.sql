f6c413b1f25d415a395127b14863c553f2ecde5d
Carter Culkin <78938422+cculkin8@users.noreply.github.com>
2021-06-02 15:52:24 -0500

    Add files via upload
    This is by no means complete, just uploading what I have so far so I don't forget to later.

---
+++ PokiQuery.sql
@@ -0,0 +1,28 @@
+﻿SELECT *
+FROM Grade
+
+
+SELECT *
+	FROM Emotion
+
+SELECT MAX(Id) NumberOfPoems
+	FROM Poem
+
+SELECT a.Name
+	FROM Author a Where Id < 77
+	ORDER BY Name 
+	
+	
+SELECT a.Name, g.Name
+	FROM Author a 
+	LEFT JOIN Grade g on a.GradeId = g.id
+		where a.Id < 77
+		ORDER BY a.Name 
+
+
+SELECT a.Name, g.Name Grade, ge.Name Gender
+	FROM Author a 
+	LEFT JOIN Grade g on a.GradeId = g.id
+	LEFT JOIN Gender ge on a.GenderId = ge.Id
+		where a.Id < 77
+		ORDER BY a.Name 
